a5149f0d042f13e4b61d8e7962c2d1f0897101d0
lauta12 <lautavali@gmail.com>
2025-10-09 14:14:35 -0300

    Agrega funciones

---
+++ src/ConsultorioMedico/dao/TurnoDAO.java
@@ -0,0 +1,25 @@
+package ConsultorioMedico.dao;
+
+import ConsultorioMedico.modelo.Turno;
+
+public class TurnoDAO {
+    public void guardarTurno(Turno turno) {
+
+    }
+
+    public void listarTurnos() {
+
+    }
+
+    public void listarPorDni(String dniPaciente) {
+
+    }
+
+    public void eliminarTurno(int id) {
+
+    }
+
+    public void actualizarEstado(int id, String nuevoEstado) {
+
+    }
+}
