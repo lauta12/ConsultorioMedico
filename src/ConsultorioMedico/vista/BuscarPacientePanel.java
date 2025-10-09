4eb15cc919e0067a61139a29ed8cd8d1e462e709
lauta12 <lautavali@gmail.com>
2025-10-09 13:43:31 -0300

    Panel para buscar un Paciente en el sistema.

---
+++ src/ConsultorioMedico/vista/BuscarPacientePanel.java
@@ -0,0 +1,82 @@
+package ConsultorioMedico.vista;
+
+import ConsultorioMedico.util.Estilos;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class BuscarPacientePanel extends JPanel {
+
+    private JTable tablaPacientes;
+    private DefaultTableModel modelo;
+    private JScrollPane scroll;
+    private JButton btnEliminar, btnCancelar, btnBuscar, btnActualizar;
+    private JLabel lblDni;
+    private JTextField txtDni;
+    private String[] columnas;
+    private JPanel panelSuperior;
+    private JPanel panelBuscar;
+    // puedo hacer que se junten esas dos, o sea, voy a listar pacientes y ahi puedo buscar por dni también
+
+    public BuscarPacientePanel() {
+        inicializarComponentes();
+        configurarLayout();
+    }
+
+    private void inicializarComponentes() {
+        columnas = new String[]{"DNI", "Nombre", "Apellido", "Teléfono", "Obra Social"};
+        lblDni = new JLabel("Buscar por DNI: ");
+        txtDni = new JTextField(20);
+        btnBuscar = new JButton("Buscar");
+        btnActualizar = new JButton("Actualizar");
+        btnEliminar = new JButton("Eliminar");
+        btnCancelar = new JButton("Cancelar");
+
+        modelo = new DefaultTableModel(columnas, 0);
+        tablaPacientes = new JTable(modelo);
+        scroll = new JScrollPane(tablaPacientes);
+    }
+
+    private void aplicarEstilos() {
+        Estilos.aplicarEstiloTextField(txtDni);
+        Estilos.aplicarEstiloLabel(lblDni);
+        Estilos.aplicarEstiloBoton(btnBuscar, btnCancelar, btnActualizar, btnEliminar);
+        Estilos.aplicarEstiloPanel(panelSuperior);
+        Estilos.aplicarEstiloTabla(tablaPacientes);
+        Estilos.aplicarEstiloScroll(scroll);
+    }
+
+    private void configurarLayout() {
+        setLayout(new BorderLayout(10,10));
+
+        panelSuperior = new JPanel(new FlowLayout(FlowLayout.LEFT, 10,10));
+
+        panelSuperior.add(lblDni);
+        panelSuperior.add(txtDni);
+        panelSuperior.add(btnBuscar);
+        panelSuperior.add(btnActualizar);
+        panelSuperior.add(btnEliminar);
+        panelSuperior.add(btnCancelar);
+
+        aplicarEstilos();
+
+        add(panelSuperior, BorderLayout.NORTH);
+        add(scroll, BorderLayout.CENTER);
+    }
+
+
+    // getters
+    public JButton getBtnBuscar() { return btnBuscar; }
+    public JButton getBtnCancelar() { return btnCancelar; }
+    public JButton getBtnActualizar() { return btnActualizar; }
+    public JButton getBtnEliminar() { return btnEliminar; }
+    public JTable getTablaPacientes() { return tablaPacientes; }
+
+
+    public JTextField getTxtDni() { return txtDni; }
+    public String getDni() { return txtDni.getText(); }
+    public DefaultTableModel getModelo() { return modelo; }
+    // setters
+
+
+}
