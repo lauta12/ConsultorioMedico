a15cefd5781cc0ced7aa368c473bbd370a9be827
lauta12 <lautavali@gmail.com>
2025-10-09 14:09:57 -0300

    Agrega vista y controlador

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import ConsultorioMedico.controlador.ConsultorioMedicoControlador;
+import ConsultorioMedico.vista.ConsultorioMedicoVista;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConsultorioMedicoVista vista = new ConsultorioMedicoVista();
+        ConsultorioMedicoControlador controlador = new ConsultorioMedicoControlador(vista);
+    }
+
+}
