33b5d3b67005b4838f2ce77b491a5af3b7d9fc4b
lauta12 <lautavali@gmail.com>
2025-10-10 13:06:46 -0300

    Agrega mas atributos con sus getters y setters

--- src/ConsultorioMedico/modelo/Doctor.java
+++ src/ConsultorioMedico/modelo/Doctor.java
@@ -1,28 +1,32 @@
 package ConsultorioMedico.modelo;
 
 public class Doctor {
-    private int id;
+    private int id_doctor;
     private String nombre;
     private String especialidad;
+    private String telefono;
+    private String email;
 
     public Doctor() {}
 
-    public Doctor(int id, String nombre, String especialidad) {
-        this.id = id;
+    public Doctor(int id, String nombre, String especialidad, String telefono, String email) {
+        this.id_doctor = id;
         this.nombre = nombre;
         this.especialidad = especialidad;
+        this.telefono = telefono;
+        this.email = email;
     }
 
-    public int getId() { return id; }
+    // getters y setters
+    public int getId_doctor() { return id_doctor; }
     public String getNombre() { return nombre; }
     public String getEspecialidad() { return especialidad; }
+    public String getTelefono() { return telefono; }
+    public String getEmail() { return email; }
 
-    public void setId(int id) { this.id = id; }
+    public void setId_doctor(int id_doctor) { this.id_doctor = id_doctor; }
     public void setNombre(String nombre) { this.nombre = nombre; }
     public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
-
-    @Override
-    public String toString() {
-        return nombre + " - " + especialidad;
-    }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public void setEmail(String email) { this.email = email; }
 }
