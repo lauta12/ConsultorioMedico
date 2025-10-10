3ff29dcb16ed628618e0ad8e36ba04a724564462
lauta12 <lautavali@gmail.com>
2025-10-10 13:09:43 -0300

    Organiza controlador

---
+++ src/ConsultorioMedico/controlador/RegistrarPacienteControlador.java
@@ -0,0 +1,54 @@
+package ConsultorioMedico.controlador;
+
+import ConsultorioMedico.dao.PacienteDAO;
+import ConsultorioMedico.modelo.Paciente;
+import ConsultorioMedico.util.Estilos;
+import ConsultorioMedico.util.Validacion;
+import ConsultorioMedico.vista.ConsultorioMedicoVista;
+import ConsultorioMedico.vista.RegistrarPacientePanel;
+
+import javax.swing.*;
+
+public class RegistrarPacienteControlador {
+   private ConsultorioMedicoVista vista;
+   private RegistrarPacientePanel panelRegistrar;
+   private Validacion validador = new Validacion(vista);
+   private PacienteDAO pacienteDAO = new PacienteDAO();
+
+    public RegistrarPacienteControlador(ConsultorioMedicoVista vista) {
+        this.vista = vista;
+        this.panelRegistrar = vista.getRegistrarPacientePanel();
+
+        // panel registrar paciente. Boton guardar.
+        vista.getRegistrarPacientePanel().getBtnGuardar().addActionListener(e -> {
+            panelRegistrar.resetBorders();
+
+            if(validador.validarDatos(panelRegistrar)) {
+                Paciente paciente = new Paciente(
+                        panelRegistrar.getNombre(),
+                        panelRegistrar.getApellido(),
+                        panelRegistrar.getDni(),
+                        panelRegistrar.getObraSocialSeleccionada(),
+                        panelRegistrar.getTelefono()
+                );
+
+                pacienteDAO.guardarPaciente(paciente);
+
+                // limpia los campos después de guardar los datos
+                Estilos.limpiarTextFields(panelRegistrar);
+            } else {
+                JOptionPane.showMessageDialog(vista, "Hay errores en el forumlario. " +
+                        "Revise nuevamente los campos.", "Error de validacion", JOptionPane.ERROR_MESSAGE);
+            }
+
+        });
+
+
+        // Botón cancelar
+        vista.getRegistrarPacientePanel().getBtnCancelar().addActionListener(e -> {
+            vista.mostrarPantalla("menu");
+            Estilos.limpiarTextFields(panelRegistrar);
+        });
+    }
+
+}
