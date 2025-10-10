6a9ef209e104b287ad9e6419669cc8e92c8ff42c
lauta12 <lautavali@gmail.com>
2025-10-10 13:10:09 -0300

    Agrega funcion para guardar turnos

--- src/ConsultorioMedico/dao/TurnoDAO.java
+++ src/ConsultorioMedico/dao/TurnoDAO.java
@@ -1,9 +1,34 @@
 package ConsultorioMedico.dao;
 
 import ConsultorioMedico.modelo.Turno;
+import ConsultorioMedico.util.Conexion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TurnoDAO {
     public void guardarTurno(Turno turno) {
+
+        String sql = "INSERT INTO turnos (dni_paciente, id_doctor, fecha, hora) VALUES (?,?,?,?,?)";
+
+        try(Connection conn = Conexion.getConexion();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, paciente.getNombre());
+            stmt.setString(2, paciente.getApellido());
+            stmt.setString(3, paciente.getDni());
+            stmt.setString(4, paciente.getObraSocial());
+            stmt.setString(5, paciente.getTelefono());
+
+            stmt.executeUpdate();
+
+            System.out.println("Paciente guardado en la base de datos");
+
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
