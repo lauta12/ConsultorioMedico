99996d9cd76be1b687dfecff4883fdfa02201d7d
lauta12 <lautavali@gmail.com>
2025-10-11 20:36:20 -0300

    Agrega Libreria FlatLaf (para modo oscuro automatico)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import ConsultorioMedico.controlador.ConsultorioMedicoControlador;
 import ConsultorioMedico.vista.ConsultorioMedicoVista;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+
+import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        FlatDarkLaf.setup();
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException(e);
+        }
+        javax.swing.UIManager.put("defaultFont", new java.awt.Font("Segoe UI", java.awt.Font.PLAIN, 16));
         ConsultorioMedicoVista vista = new ConsultorioMedicoVista();
         ConsultorioMedicoControlador controlador = new ConsultorioMedicoControlador(vista);
+
         controlador.iniciar();
     }
 
