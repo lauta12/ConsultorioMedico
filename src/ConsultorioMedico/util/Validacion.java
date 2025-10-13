8f1d827574801a88dbad049075af808b963d94d0
lauta12 <lautavali@gmail.com>
2025-10-13 15:28:55 -0300

    Elimina import innecesario

--- src/ConsultorioMedico/util/Validacion.java
+++ src/ConsultorioMedico/util/Validacion.java
@@ -22,8 +22,6 @@ public class Validacion {
     public boolean validarDatos(RegistrarPacientePanel panel) {
         boolean valido = true;
 
-        panel.limpiarErrores();
-
         String nombre = panel.getTxtNombre().getText();
         String apellido = panel.getTxtApellido().getText();
         String dni = panel.getTxtDni().getText();
@@ -31,40 +29,30 @@ public class Validacion {
         String obraSocial = panel.getObraSocialSeleccionada();
 
         if (nombre.trim().isEmpty() || !nombre.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) {
-            marcarError(panel.getTxtNombre());
             valido = false;
         } else {
             panel.setNombre(capitalizarNombre(nombre));
         }
 
         if (apellido.trim().isEmpty() || !apellido.matches("^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$")) {
-            marcarError(panel.getTxtApellido());
             valido = false;
         } else {
             panel.setApellido(capitalizarNombre(apellido));
         }
 
-
         if(dni.trim().isEmpty() || !dni.trim().matches("\\d{8}")) {
-            marcarError(panel.getTxtDni());
             valido = false;
         }
 
         if(telefono.trim().isEmpty() || !telefono.matches("\\d+") || telefono.length() < 8) {
-            marcarError(panel.getTxtTelefono());
             valido = false;
         }
 
         if(obraSocial == null || obraSocial.trim().isEmpty() || obraSocial.equals("Seleccionar obra social")) {
-            marcarError(panel.getComboObraSocial());
             valido = false;
         }
 
         return valido;
-    }
-
-    private void marcarError(JComponent campo) {
-        campo.setBorder(new LineBorder(Color.RED, 2));
     }
 
     public static String capitalizarNombre(String texto) {
@@ -72,7 +60,7 @@ public class Validacion {
             return "";
         }
 
-        // Pasamos todo a minúsculas primero
+        // Pasamos to/do a minúsculas primero
         texto = texto.toLowerCase().trim();
 
         // Dividimos por espacios (por si hay más de un nombre/apellido)
@@ -104,6 +92,5 @@ public class Validacion {
             modelo.addRow(fila);
         }
     }
-
 
 }
